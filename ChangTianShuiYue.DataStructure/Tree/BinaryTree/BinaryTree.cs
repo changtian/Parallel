90570859df5fab9f8bb480eed692a6656896f18c
Joey <1196824358@qq.com>
2016-05-29 16:31:53 +0800

    增加新数据结构以及单元测试
    1. 增加二叉树的简单实现
    2. 增加对 parallel 类库的单元测试

---
+++ ChangTianShuiYue.DataStructure/Tree/BinaryTree/BinaryTree.cs
@@ -0,0 +1,41 @@
+﻿using ChangTianShuiYue.DataStructure.Node;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangTianShuiYue.DataStructure.Tree.BinaryTree
+{
+    public abstract class BinaryTree<T> where T:class
+    {
+        private BinaryTreeNode<T> root;
+
+        public BinaryTree() 
+        {
+            this.root = null;
+        }
+
+        public BinaryTree(BinaryTreeNode<T> data)
+        {
+            this.root = data;
+        }
+
+        public void Clear()
+        {
+            this.root = null;
+        }
+
+        public BinaryTreeNode<T> Root
+        {
+            get 
+            {
+                return root;
+            }
+            set 
+            {
+                root = value;
+            }
+        }
+    }
+}
---
+++ ParallelExtensionExtrasTest/CoordinationDataStructures/AsyncCoordination/AsyncCallTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ParallelExtensionExtrasTest.CoordinationDataStructures.AsyncCoordination
+{
+    [TestClass]
+    public class AsyncCallTest
+    {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void PostTest()
+        {
+            AsyncCall<string> stringAsync = AsyncCall.Create<string>(GetAction("A"), 5, 2, TaskScheduler.Default);
+
+            stringAsync.Post("Hello");
+            stringAsync.Post("World");
+            stringAsync.Post("This is AI speaking.");
+            stringAsync.Post("A");
+            stringAsync.Post("B");
+            stringAsync.Post("C");
+            stringAsync.Post("D");
+            stringAsync.Post("E");
+            stringAsync.Post("F");
+            stringAsync.Post("G");
+        }
+
+        [TestMethod]
+        public void PostAsyncTest()
+        {
+            AsyncCall<string> sAsync = AsyncCall.Create<string>(new Func<string, Task>((s) =>
+            {
+                return new TaskFactory().StartNew(() => 
+                {
+                    System.Diagnostics.Debug.WriteLine("Task is Processing string: " + s);
+                });
+            }), 5, TaskScheduler.Default);
+
+
+            sAsync.Post("Hello");
+            sAsync.Post("World");
+            sAsync.Post("This is AI speaking.");
+            sAsync.Post("A");
+            sAsync.Post("B");
+            sAsync.Post("C");
+            sAsync.Post("D");
+            sAsync.Post("E");
+            sAsync.Post("F");
+            sAsync.Post("G");
+        }
+
+        public Action<string> GetAction(string actionName)
+        {
+            Action<string> task = new Action<string>((s) =>
+            {
+                if (s == "B")
+                    Thread.Sleep(1000);
+                System.Diagnostics.Debug.WriteLine("Task"+ actionName+" is Processing string: " + s);
+            });
+
+            return task;
+        }
+
+        private void HandleString(string s)
+        { }
+    }
+}
