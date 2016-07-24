62385e8abf98f9be87d9d0b85a445ecd6c16bfdb
Joey <1196824358@qq.com>
2016-07-24 12:55:21 +0800

    Test for MaxDegreeOfParallelism
    最大并行测试

---
+++ ParallelExtensionExtrasTest/ThreadingTest/MaxDegreeOfParallelismTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelExtensionExtrasTest.ThreadingTest
+{
+    [TestClass]
+    public class MaxDegreeOfParallelismTest
+    {
+        [TestMethod]
+        public void TestParallelism()
+        {
+            var locker = new Object();
+            int count = 0;
+            Parallel.For
+                (0
+                 , 1000
+                 , new ParallelOptions { MaxDegreeOfParallelism = 8 }
+                 , (i) =>
+                 {
+                     Interlocked.Increment(ref count);
+                     lock (locker)
+                     {
+                         System.Diagnostics.Debug.WriteLine("Number of active threads:" + count);
+                         Thread.Sleep(10);
+                     }
+                     Interlocked.Decrement(ref count);
+                 }
+                );
+        }
+    }
+}
